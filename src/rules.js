e6294b9fc85bbf62f0374f227a49db2ced978e42
SilencerWeb <silencer.web@gmail.com>
2018-11-15 17:55:49 +0500

    Change rules' examples a little bit

--- src/rules.js
+++ src/rules.js
@@ -4,12 +4,13 @@ export const rules = [
     shortDescription: 'Enforce “for” loop update clause moving the counter in the right direction',
     longDescription: 'A for loop with a stop condition that can never be reached, such as one with a counter that moves in the wrong direction, will run infinitely. While there are occasions when an infinite loop is intended, the convention is to construct such loops as while loops. More typically, an infinite for loop is a bug.',
     examples: {
-      correct: `for (var i = 0; i < 10; i++) {}`,
-      incorrect: `for (var i = 0; i < 10; i--) {
-}
+      correct: `// 1
+for (var i = 0; i < 10; i++) {}`,
+      incorrect: `// 1
+for (var i = 0; i < 10; i--) {}
 
-for (var i = 10; i >= 0; i++) {
-}`,
+// 2
+for (var i = 10; i >= 0; i++) {}`,
     },
     isActive: true,
     isChecked: false,
@@ -19,7 +20,8 @@ for (var i = 10; i >= 0; i++) {
     shortDescription: 'Disallow using an async function as a Promise executor',
     longDescription: 'This rule aims to disallow async Promise executor functions.',
     examples: {
-      correct: `const result = new Promise((resolve, reject) => {
+      correct: `// 1
+const result = new Promise((resolve, reject) => {
   readFile('foo.txt', function(err, result) {
     if (err) {
       reject(err);
@@ -29,8 +31,10 @@ for (var i = 10; i >= 0; i++) {
   });
 });
 
+// 2
 const result = Promise.resolve(foo);`,
-      incorrect: `const result = new Promise(async (resolve, reject) => {
+      incorrect: `// 1
+const result = new Promise(async (resolve, reject) => {
   readFile('foo.txt', function(err, result) {
     if (err) {
       reject(err);
@@ -40,6 +44,7 @@ const result = Promise.resolve(foo);`,
   });
 });
 
+// 2
 const result = new Promise(async (resolve, reject) => {
   resolve(await foo);
 });`,
@@ -52,21 +57,24 @@ const result = new Promise(async (resolve, reject) => {
     shortDescription: 'Disallow await inside of loops',
     longDescription: 'This rule disallows the use of await within loop bodies.',
     examples: {
-      correct: `async function foo(things) {
+      correct: `// 1
+async function foo(things) {
   const results = [];
+  
   for (const thing of things) {
-    // Good: all asynchronous operations are immediately started.
     results.push(bar(thing));
   }
-  // Now that all the asynchronous operations are running, here we wait until they all complete.
+  
   return baz(await Promise.all(results));
 }`,
-      incorrect: `async function foo(things) {
+      incorrect: `// 1
+async function foo(things) {
   const results = [];
+  
   for (const thing of things) {
-    // Bad: each loop iteration is delayed until the entire asynchronous operation completes
     results.push(await bar(thing));
   }
+  
   return baz(results);
 }`,
     },
@@ -78,16 +86,13 @@ const result = new Promise(async (resolve, reject) => {
     shortDescription: 'Disallow comparing against -0',
     longDescription: 'The rule should warn against code that tries to compare against -0, since that will not work as intended. That is, code like x === -0 will pass for both +0 and -0. The author probably intended Object.is(x, -0).',
     examples: {
-      correct: `if (x === 0) {
-    // doSomething()...
-}
+      correct: `// 1
+if (x === 0) {}
 
-if (Object.is(x, -0)) {
-    // doSomething()...
-}`,
-      incorrect: `if (x === -0) {
-    // doSomething()...
-}`,
+// 2
+if (Object.is(x, -0)) {}`,
+      incorrect: `// 1
+if (x === -0) {}`,
     },
     isActive: false,
     isChecked: false,
@@ -97,10 +102,16 @@ if (Object.is(x, -0)) {
     shortDescription: 'Disallow control characters in regular expressions',
     longDescription: 'This rule disallows control characters in regular expressions.',
     examples: {
-      correct: `var pattern1 = /\x20/;
-var pattern2 = new RegExp("\x20");`,
-      incorrect: `var pattern1 = /\x1f/;
-var pattern2 = new RegExp("\x1f");`,
+      correct: `// 1
+var pattern = /\x20/;
+
+// 2
+var pattern = new RegExp("\x20");`,
+      incorrect: `// 1
+var pattern = /\x1f/;
+
+// 2
+var pattern = new RegExp("\x1f");`,
     },
     isActive: false,
     isChecked: false,
@@ -110,10 +121,12 @@ var pattern2 = new RegExp("\x1f");`,
     shortDescription: 'Disallow the use of debugger',
     longDescription: 'This rule disallows debugger statements.',
     examples: {
-      correct: `function isTruthy(x) {
+      correct: `// 1
+function isTruthy(x) {
     return Boolean(x); // set a breakpoint at this line
 }`,
-      incorrect: `function isTruthy(x) {
+      incorrect: `// 1
+function isTruthy(x) {
     debugger;
     return Boolean(x);
 }`,
